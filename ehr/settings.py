585d1ef9aa0eacea166fad79550c9ce5a3221a50
Mihailo <test@gmail.com>
2025-06-24 23:24:30 +0200

    Small fix

--- ehr/settings.py
+++ ehr/settings.py
@@ -177,7 +177,8 @@ GRAPHQL_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://web-production-30b7.up.railway.app"
+    "https://web-production-30b7.up.railway.app",
+    "https://ehr-frontend-rho.vercel.app",
 ]
 
 # CSRF Settings for Railway
@@ -185,6 +186,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://web-production-30b7.up.railway.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://ehr-frontend-rho.vercel.app",
 ]
 
 # Custom User Model
